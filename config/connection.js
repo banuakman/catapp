9cea3baa1d099b624210064dc28abe8d2d10260a
banuakman <banu@asterya.com>
2021-02-19 13:39:55 -0500

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 let connection;
-if (process.env.JAWSDB.URL) {
-  connection = mysql.createConnection(process.env.JAWSDB.URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
